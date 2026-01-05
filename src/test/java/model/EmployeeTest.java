813b921eb30c765a076feacfbdc82abbdac4a9a9
duvan castro <duvancastro1026@gmail.com>
2026-01-05 08:34:04 +0100

    create test Constructor_Falla_Si_Firstname_Es_Nulo

--- src/test/java/model/EmployeeTest.java
+++ src/test/java/model/EmployeeTest.java
@@ -3,6 +3,7 @@ package model;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EmployeeTest {
 
@@ -17,4 +18,14 @@ public class EmployeeTest {
         }
     }
 
+    @Test
+    void Constructor_Falla_Si_Firstname_Es_Nulo()
+    {
+        try {
+            Employee duvanPerez = new Employee(null, "Perez", "Gomez", "12345678A", 1000);
+            assertTrue(false);
+        } catch (IllegalArgumentException  e) {
+            assertTrue(e.getMessage().equals("FirstName is null"));
+        }
+    }
 }
