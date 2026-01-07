cdc0587490f720ca27e8a2b8cd6a351601d2b5e3
duvan castro <duvancastro1026@gmail.com>
2026-01-07 21:20:44 +0100

    create test for validation salary parameter

--- src/test/java/model/EmployeeTest.java
+++ src/test/java/model/EmployeeTest.java
@@ -119,4 +119,37 @@ public class EmployeeTest {
         }
     }
 
+    @Test
+    void Constructor_Falla_Si_Salary_Es_Menor_Que_600()
+    {
+        try {
+            Employee duvanPerez = new Employee("juan", "Perez", "Gomez", "12345678A", 500);
+            assertTrue(false);
+        } catch (IllegalArgumentException  e) {
+            assertTrue(e.getMessage().equals("Salary fuera de rango"));
+        }
+    }
+
+    @Test
+    void Constructor_Falla_Si_Salary_Es_Mayor_Que_10000()
+    {
+        try {
+            Employee duvanPerez = new Employee("juan", "Perez", "Gomez", "12345678A", 15000);
+            assertTrue(false);
+        } catch (IllegalArgumentException  e) {
+            assertTrue(e.getMessage().equals("Salary fuera de rango"));
+        }
+    }
+
+    @Test
+    void Constructor_Crea_Employee_Si_Salary_Es_600_O_10000()
+    {
+        try {
+            Employee duvanPerez = new Employee("juan", "Perez", "Gomez", "12345678A", 600);
+            assertTrue(true);
+        } catch (IllegalArgumentException  e) {
+            assertTrue(false);
+        }
+    }
+
 }
